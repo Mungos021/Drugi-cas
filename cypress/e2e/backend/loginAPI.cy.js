c129d761c808e137d73476abba7da7e674001971
Marko Djuric <marko.djuric1303@gmail.com>
2023-03-28 16:42:19 +0200

    priprema za cetvrti cas

---
+++ cypress/e2e/backend/loginAPI.cy.js
@@ -0,0 +1,11 @@
+/// <reference types="Cypress" />
+
+describe('Login via API', () => {
+    before(() => {
+        cy.clearAllCookies()
+        cy.clearAllLocalStorage()
+        cy.clearAllSessionStorage()
+    });
+
+
+});
---
+++ cypress/page_object/createGalleryPOM.js
@@ -0,0 +1,33 @@
+class CreateGalleryPage {
+
+    get titleHeading() {
+        return cy.get('h1[class="title-style"]');
+    }
+    get titleInputField() {
+        return cy.get('input[id="title"]');
+    }
+    get descriptionInputField() {
+        return cy.get('input[id="description"]');
+    }
+    get imageInputField() {
+        return cy.get('input[type="url"]');
+    }
+    get buttonUp() {
+        return cy.get('i[class="fas fa-chevron-circle-up"]');
+    }
+    get buttonDown() {
+        return cy.get('i[class="fas fa-chevron-circle-down"]');
+    }
+    get addImageBtn() {
+        return cy.get('//*[@id="app"]/div[2]/div/div/form/div[3]/button');
+    }
+    get submitBtn() {
+        return cy.get('button').contains('Submit');
+    }
+    get cancelBtn() {
+        return cy.get('button').contains('Cancel');
+    }
+
+}
+
+export const createGalleryPage = new CreateGalleryPage();
